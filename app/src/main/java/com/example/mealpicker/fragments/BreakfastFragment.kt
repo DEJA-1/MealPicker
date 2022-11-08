62452bf546323c478887180fe853569be11915f9
DEJA1 <allowestq@gmail.com>
2022-11-08 10:22:43 +0100

    setting up fab

--- app/src/main/java/com/example/mealpicker/fragments/BreakfastFragment.kt
+++ app/src/main/java/com/example/mealpicker/fragments/BreakfastFragment.kt
@@ -7,10 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContentProviderCompat
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mealpicker.R
 import com.example.mealpicker.adapter.BreakfastAdapter
 import com.example.mealpicker.data.Datasource
 import com.example.mealpicker.databinding.FragmentBreakfastBinding
+import com.example.mealpicker.model.Breakfast
 
 class BreakfastFragment : Fragment() {
 
@@ -35,6 +38,18 @@ class BreakfastFragment : Fragment() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
+        //fab button
+        binding.fabBreakfast.setOnClickListener { manageRandomBreakfast(breakfastList) }
+
+    }
+
+    private fun manageRandomBreakfast(breakfastList : List<Breakfast>) {
+        drawBreakfast(breakfastList)
+//        findNavController().navigate(R.id.)
+    }
+
+    private fun drawBreakfast(breakfastList: List<Breakfast>) : Breakfast {
+        return breakfastList.random()
     }
 
 }
